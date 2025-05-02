99388bb4a45c55ae3863c7fe091f8d54a7c3f1d3
Joochan <joochan123123@gmail.com>
2025-05-03 03:36:29 +0900

    feat: SkeletalMesh 기본 데이터 형태 정의

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Rendering/Mesh/SkeletalMeshRenderData.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Rendering/Mesh/SkeletalMeshRenderData.h
@@ -1,9 +1,29 @@
 #pragma once
 
+#include "HAL/PlatformType.h"
+#include "Container/Array.h"
+
+#include "Rendering/Types/Buffers.h"
+
+struct FSkeletalMeshRenderSection
+{
+    // int32 BoneIndex; // 이 섹션에서 사용하는 본 인덱스. -1이면 사용하지 않음.
+
+    uint32 BaseIndex; 
+    uint32 NumTriangles;
+    TArray<uint16> BoneMap; // 이 섹션에서 사용하는 본 모음.
+    uint16 MaterialIndex; // 이 섹션에서 사용하는 머티리얼 인덱스.
+    uint32 BaseVertexIndex; // 이 섹션에서 사용하는 버텍스 인덱스.
+
+    FVertexInfo VertexInfo; // 버텍스 정보
+    FIndexInfo IndexInfo; // 인덱스 정보
+
+};
+
 class FSkeletalMeshRenderData
 {
 public:
-    
+    TArray<FSkeletalMeshRenderSection> RenderSections; // 렌더링 섹션 모음.
 
 };
 
